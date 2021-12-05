3332b32de9eaf4e3b88e2bd943af710919b41ed9
Karthik Mohanasundaram <connect2kms@outlook.com>
2021-12-05 17:17:42 -0500

    Added service for unsplash

--- src/app/fetch.service.ts
+++ src/app/fetch.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FetchService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getPhoto() {
+    return this.http.get('https://api.unsplash.com/photos/random', {
+      headers: {
+        Authorization: 'Client-ID tWvMLParu98NnY9NPUQeCeZNfEC6kDzrldMuhKDs3Wg',
+      },
+    });
+  }
 }
